2a6e9d7b8e4ea0999161e54ec50e152e5280e070
apozel <52205094+apozel@users.noreply.github.com>
2019-11-04 00:27:32 +0100

    Create Demande.java

---
+++ src/Demande.java
@@ -0,0 +1,32 @@
+
+/**
+ * Demande
+ */
+public class Demande {
+
+    private Diagnostic diag;
+    private Patient patientConsernee;
+
+    public Demande(Diagnostic diag, Patient patientConsernee) {
+        this.diag = diag;
+        this.patientConsernee = patientConsernee;
+    }
+
+    public Diagnostic getDiag() {
+        return diag;
+    }
+
+    public void setDiag(Diagnostic diag) {
+        this.diag = diag;
+    }
+
+    public Patient getPatientConsernee() {
+        return patientConsernee;
+    }
+
+    public void setPatientConsernee(Patient patientConsernee) {
+        this.patientConsernee = patientConsernee;
+    }
+
+    
+}
