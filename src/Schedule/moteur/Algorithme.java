9dbdc0f69bd07a8ebe5534751947bf7f6b16034b
apozel <52205094+apozel@users.noreply.github.com>
2020-01-16 00:36:08 +0100

    Revert "Revert "le temps marche mais modife aussi les prochaines jours""
    This reverts commit 442d58a949367ad61482c3e496353f9a9cbe17e1.

--- src/Schedule/moteur/Algorithme.java
+++ src/Schedule/moteur/Algorithme.java
@@ -2,7 +2,6 @@ package Schedule.moteur;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +22,8 @@ public class Algorithme {
 
     private JunctionInformation AccesStockageInformation;
     private Docteur docChoisit;
+    private LocalDateTime momentOuOnRegarde;
+    private List<RendezVous> rdvDuJour;
 
     public Algorithme(JunctionInformation lienAvecBdd) {
 
@@ -41,19 +42,19 @@ public class Algorithme {
 
         System.out.println(" Algorithme : ajouterRendezVous() : " + nouvelleDemandeATraiter);
         List<RendezVous> rdvReturn = new ArrayList<RendezVous>();
-        LocalDateTime jourOuOnRegarde = getDateEtHeure();
+        this.momentOuOnRegarde = getDateEtHeure();
 
         this.docChoisit = choixDocteur();
         // TODO changer l'heure de debut en fonction de ou commence les modifcation
-        LocalTime heureDebut = donnerHeure(jourOuOnRegarde);
+        this.rdvDuJour = avoirLesRendezVousDejaDonnee(momentOuOnRegarde, docChoisit);
+        checkHoraires();
         // TODO changer la fonction pour renvoyer les rendez vous futur de la journee et
         // pas toute la journee
-        List<RendezVous> rdvDuJour = avoirLesRendezVousDejaDonnee(jourOuOnRegarde, docChoisit);
 
         Noeud[] transformationPositionNoeud = new Noeud[rdvDuJour.size() + 2];
         transformationPositionNoeud[0] = new Noeud(0, docChoisit.getLieuDeDepart());
         for (int i = 0; i < rdvDuJour.size(); i++) {
-            transformationPositionNoeud[i + 1] = new Noeud(i, rdvDuJour.get(i).getMalade().getLieuDeVie());
+            transformationPositionNoeud[i + 1] = new Noeud(i + 1, rdvDuJour.get(i).getMalade().getLieuDeVie());
         }
         transformationPositionNoeud[rdvDuJour.size() + 1] = new Noeud(rdvDuJour.size() + 1,
                 nouvelleDemandeATraiter.getMalade().getLieuDeVie());
@@ -61,31 +62,31 @@ public class Algorithme {
         Noeud positionApresAlgo[] = Calcul.l2(transformationPositionNoeud);
 
         for (int i = 1; i < positionApresAlgo.length; i++) {
+
+            checkHoraires();
+
             for (RendezVous rdvDejaPresent : rdvDuJour) {
 
                 if (rdvDejaPresent.equalsPosition(positionApresAlgo[i])) { // positif si la position correspond au
                     // rendez vous
-                    rdvDejaPresent.setDate(jourOuOnRegarde.toLocalDate());
-                    rdvDejaPresent.setHeureDebut(heureDebut);
+                    rdvDejaPresent.setDate(momentOuOnRegarde.toLocalDate());
+                    rdvDejaPresent.setHeureDebut(momentOuOnRegarde.toLocalTime());
                     rdvReturn.add(rdvDejaPresent);
                     break;
-                } else if (nouvelleDemandeATraiter.getMalade().getLieuDeVie().equalsPosition(positionApresAlgo[i])) { // c'est
-                                                                                                                      // le
-                                                                                                                      // cas
-                                                                                                                      // de
-                                                                                                                      // la
-                                                                                                                      // nouvelle
-                                                                                                                      // demande
-                    rdvReturn.add(new RendezVous(jourOuOnRegarde.toLocalDate(), heureDebut, Duration.ofMinutes(45),
-                            docChoisit, nouvelleDemandeATraiter.getMalade(), nouvelleDemandeATraiter.getDiag()));
+                } else if (nouvelleDemandeATraiter.getMalade().getLieuDeVie().equalsPosition(positionApresAlgo[i])) {
+                    // c'est le cas de la nouvelle demande
+                    rdvReturn.add(new RendezVous(momentOuOnRegarde.toLocalDate(), momentOuOnRegarde.toLocalTime(),
+                            Duration.ofMinutes(45), docChoisit, nouvelleDemandeATraiter.getMalade(),
+                            nouvelleDemandeATraiter.getDiag()));
                     break;
                 }
             }
 
             // TODO changer le temps en fonction de la duree de la consulatation au dessus
             // pareil pour les heures de pauses
-            heureDebut = heureDebut.plus(Duration.ofMinutes(45));
+            momentOuOnRegarde = momentOuOnRegarde.plus(Duration.ofMinutes(45));
             // TODO si le rdv depasse les horaires du medecin passer a un autre jour
+            
 
         }
 
@@ -99,22 +100,68 @@ public class Algorithme {
 
         // TODO changer le choix du docteur en fonction de la situation ou de son
         // occupation
+        // il faudra un aqlgorithme pour regrouper les rdv geographiquement en fonction
+        // du medecin (de sa zone de depart)
 
         return docteurs.get(0);
     }
 
-    public LocalTime donnerHeure(LocalDateTime jourChoisit) {
-        // TODO changer l'heure de debut en fonction de ou commence les modifcation
-        /*
-         * if (jourChoisit.toLocalTime().isAfter(docChoisit.getHoraires(0))) { return
-         * jourChoisit.toLocalTime(); }
-         */
-        return docChoisit.getHoraires(0);
+    /*
+     * donner heure explications cas possibles : ( heure de la demande =
+     * jourOuOnRegarde ) : - si il y a des rendez-vous deja donnée : - les rendez
+     * vous qui vont apparaitre dans la liste sont ceux apres le jour ou on regarde
+     * donc il faut juste donner l'heure du premier rendez vous et si les rendez
+     * vous sont tous donnees jusqua la fin du service passé sur la journee suivante
+     * - si aucun rendez-vous ai donnée : (fonction pour donner l'heure de plusieur
+     * rendez vous) - si l'heure de la demande est entre minuit et l'heure de debut
+     * du medecin alors on commence a l'heure du medecin(0) - si l'heure de la
+     * demande est apres l'heure de debut du medecin mais avant la pause du midi
+     * alors on prend en compte l'heure de la demande avec un delai de 30 minutes
+     * (pour le deplacement) - si l'heure de la demande est dans la pause du midi
+     * alors on choisit le rdv a lheure de la reprise - si l'heure de la demande est
+     * apres l'heure de debut d'apres midi du medecin mais avant l'arret du soir
+     * alors on prend en compte l'heure de la demande avec un delai de 30 minutes
+     * (pour le deplacement) - si l'heure de la demande est apres l'arret de travail
+     * du medecin alors la date doit etre changer au lendemain il en est de meme
+     * pour la liste des rendez-vous donnée (et on return donnerHeure du jour
+     * suivant )
+     */
+    public void checkHoraires() {
+
+        if (this.momentOuOnRegarde.toLocalTime().isBefore(this.docChoisit.getHoraires(0))) {
+            this.momentOuOnRegarde = LocalDateTime.of(momentOuOnRegarde.toLocalDate(), this.docChoisit.getHoraires(0));
+        } else if (this.momentOuOnRegarde.toLocalTime().isAfter(this.docChoisit.getHoraires(1))
+                && this.momentOuOnRegarde.toLocalTime().isBefore(this.docChoisit.getHoraires(2))) {
+            this.momentOuOnRegarde = LocalDateTime.of(momentOuOnRegarde.toLocalDate(), this.docChoisit.getHoraires(2));
+        } else if (this.momentOuOnRegarde.toLocalTime().isAfter(this.docChoisit.getHoraires(3))) {
+            this.momentOuOnRegarde = LocalDateTime.of(momentOuOnRegarde.toLocalDate().plusDays(1),
+                    this.docChoisit.getHoraires(0));
+        }
+
+    }
+
+    public void checkSiLaJourneeEstPleine(){
+       for (RendezVous rendezVous : rdvDuJour) {
+           if (rendezVous.getHeureDebut().plus(rendezVous.getDureeConsultation()).isAfter(this.docChoisit.getHoraires(3))) {
+            this.momentOuOnRegarde = LocalDateTime.of(momentOuOnRegarde.toLocalDate().plusDays(1),
+                    this.docChoisit.getHoraires(0));
+       } 
     }
 
     public List<RendezVous> avoirLesRendezVousDejaDonnee(LocalDateTime dateDemander, Docteur docteurChoisit) {
         return AccesStockageInformation.getRendezVousDuJour(dateDemander, docteurChoisit);
     }
+
+    public List<RendezVous> rendezvousDejaDonneeAPartirDunMoment(LocalDateTime dateDemander, Docteur docteurChoisit) {
+        List<RendezVous> retour = new ArrayList<RendezVous>();
+        for (RendezVous rendezVous : AccesStockageInformation.getRendezVousDuJour(dateDemander, docteurChoisit)) {
+            if (rendezVous.getHeureDebut().isAfter(dateDemander.toLocalTime())) {
+                retour.add(rendezVous);
+            }
+        }
+        return retour;
+    }
+
 
     public List<Docteur> avoirLesDocteurDisponible() {
         return AccesStockageInformation.getDocteurDisponible();
