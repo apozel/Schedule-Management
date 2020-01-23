06765070d5410456e8ad136dcaa3d7f881bacead
apozel <52205094+apozel@users.noreply.github.com>
2020-01-23 12:20:54 +0100

    debut des test pour Gson

--- src/Schedule/enregistrement/testEnregistrement.java
+++ src/Schedule/enregistrement/testEnregistrement.java
@@ -2,19 +2,21 @@ package Schedule.enregistrement;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.time.Duration;
 import java.time.LocalDateTime;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import Schedule.utilities.Diagnostic;
 import Schedule.utilities.Docteur;
 import Schedule.utilities.Patient;
 import Schedule.utilities.Position;
 import Schedule.utilities.RendezVous;
+
+import com.google.gson.*;
+
 
 /**
  * testEnregistrement
@@ -24,6 +26,7 @@ public class testEnregistrement {
     private File fichier = new File("C:/Users/tomch/Documents/java/Schedule-Management/src/Ressources/Json/test.json");
     private FileWriter ecritureStream = null;
     private LocalDateTime DateHeureActuel = LocalDateTime.now();
+    private Gson trad = new GsonBuilder().setPrettyPrinting().create();
     private Docteur testDocteur = new Docteur("arthur", "saucisson", "mentoniste", new Position(90, 70));
     private Patient testPatient = new Patient(new Position(11, 3), "Rafael", "le clown");
     private RendezVous testRendezVous = new RendezVous(DateHeureActuel.toLocalDate(),
@@ -31,52 +34,37 @@ public class testEnregistrement {
             new Diagnostic(1, "gastro", testPatient));
 
 
-    public testEnregistrement() {
+   
+
+    public void testCreationGson() {
+        InputStream myStream = new FileInputStream("C:/Users/tomch/Documents/java/Schedule-Management/src/Ressources/Json/test.json");
+String myString = IOUtils.toString(myStream);
+    }
+
+    public void simpleGsonTest(){
+        ecritureFichier(trad.toJson(testRendezVous));
+    }
+
+    public void ecritureFichier(String aEcrire){
         try {
-            this.ecritureStream = new FileWriter(fichier);
-            // this.fos =new FileOutputStream(fichier);
-        } catch (Exception e) {
-            System.out.println("erreur");
-        }
-
-    }
-
-    public void writeJsonSimpleDemo() throws Exception {
-        // First Employee
-        JSONObject employeeDetails = new JSONObject();
-        employeeDetails.put("firstName", "Lokesh");
-        employeeDetails.put("lastName", "Gupta");
-        employeeDetails.put("website", "howtodoinjava.com");
-
-        JSONObject employeeObject = new JSONObject();
-        employeeObject.put("employee", employeeDetails);
-
-        // Second Employee
-        JSONObject employeeDetails2 = new JSONObject();
-        employeeDetails2.put("firstName", "Brian");
-        employeeDetails2.put("lastName", "Schultz");
-        employeeDetails2.put("website", "example.com");
-
-        JSONObject employeeObject2 = new JSONObject();
-        employeeObject2.put("employee", employeeDetails2);
-
-        // Add employees to list
-        JSONArray employeeList = new JSONArray();
-        employeeList.put(employeeObject);
-        employeeList.put(employeeObject2);
-
-        this.ecritureStream.write(employeeList.toString());
+            this.ecritureStream.write(aEcrire);
         this.ecritureStream.flush();
+        } catch (Exception e) {
+            
+        }
+        
     }
+
+  
 
     public static void main(String[] args) {
         testEnregistrement test = new testEnregistrement();
 
         try {
-            test.writeJsonSimpleDemo();
+            test.simpleGsonTest();
 
         } catch (Exception e) {
-            // TODO: handle exception
+            
         }
 
     }
